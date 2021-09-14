5e400ddb1c218dd7bf111b4f328faa14b6a5b3d8
Darren Ford <dford_au-reg@yahoo.com>
2021-09-14 10:18:18 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DSFPagerControl
+
+A description of this package.
---
+++ Tests/DSFPagerControlTests/DSFPagerControlTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DSFPagerControl
+
+final class DSFPagerControlTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DSFPagerControl().text, "Hello, World!")
+    }
+}
